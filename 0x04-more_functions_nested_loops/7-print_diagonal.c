3ded3eec3d2b51b17e5255484e7eea82670de5f7
ahmedsobhy33 <ahmedgadoa3@gmail.com>
2023-03-23 16:32:43 -0700

    second time

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,18 +10,15 @@
 
 void print_diagonal(int n)
 {
-	int i, space;
+	int i;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
 		for (i = 1 ; i <= n ; i++)
-		{
-			for (space = 1 ; space <= i ; space++)
-				_putchar(' ');
-			_putchar('/');
-			_putchar('\n');
-		}
+			_putchar(' ');
+		_putchar('/');
+		_putchar('\n');
 	}
 }
