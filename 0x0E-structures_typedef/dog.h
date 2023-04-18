e589d8b76b4a1ec545f6ab3abb7cdbb4ff957838
ahmedsobhy33 <ahmedgadoa3@gmail.com>
2023-04-18 13:58:37 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - struct for dog info
+ * @name: type char
+ * @age: float type
+ * @owner: char type
+ *
+ *Description: structure to store dog info
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
